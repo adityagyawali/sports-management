3653c8b8c77a638fde877124243e66f2a80cc877
Aditya Gyawali <gyawali_aditya@hotmail.com>
2018-11-26 15:40:59 +0200

    css change

--- src/components/SportSection/index.js
+++ src/components/SportSection/index.js
@@ -1,69 +1,53 @@
-import React, {Fragment} from 'react'
-import { Container,Card, Icon, Image } from 'semantic-ui-react'
-import Football from  '../../assets/football.jpg'
-import basketball from  '../../assets/basketball.jpg'
-import icehockey from  '../../assets/icehockey.jpg'
+import React from "react";
+import { Container, Card, Image } from "semantic-ui-react";
+import Football from "../../assets/football.jpg";
+import basketball from "../../assets/basketball.jpg";
+import icehockey from "../../assets/icehockey.jpg";
 import "./index.css";
 
-
-
-
-
 const Sport = () => (
-  <Fragment style={{ display: 'inline-block'}}>
+	<div className="sports">
+		<Card>
+			<Container>
+				<Image className="basketball" src={basketball} />
+			</Container>
+			<Card.Content>
+				<Card.Header />
+				<Card.Meta>
+					<span className="basketball" />
+				</Card.Meta>
+				<Card.Description>BASKETBALL</Card.Description>
+			</Card.Content>
+			<Card.Content extra />
+		</Card>
+		<Card>
+			<Container>
+				<Image src={icehockey} />
+			</Container>
+			<Card.Content>
+				<Card.Header />
+				<Card.Meta>
+					<span className="icehockey" />
+				</Card.Meta>
+				<Card.Description>ICEHOCKEY</Card.Description>
+			</Card.Content>
+			<Card.Content extra />
+		</Card>
 
-  <Card>
+		<Card>
+			<Container>
+				<Image src={Football} />
+			</Container>
+			<Card.Content>
+				<Card.Header />
+				<Card.Meta>
+					<span className="football" />
+				</Card.Meta>
+				<Card.Description>FOOTBALL</Card.Description>
+			</Card.Content>
+			<Card.Content extra />
+		</Card>
+	</div>
+);
 
-    <Container style={{ display: 'inline-block'}}>
-      <Image src={basketball} />
-    </Container>
-    <Card.Content>
-      <Card.Header></Card.Header>
-      <Card.Meta>
-        <span className='basketball'></span>
-      </Card.Meta>
-      <Card.Description>basketball</Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-    
-    </Card.Content>
-
-
-  </Card>
-  <Card>
-  <Container style={{ display: 'inline-block'}}>
-    <Image src={icehockey} />
-  </Container>
-  <Card.Content>
-    <Card.Header></Card.Header>
-    <Card.Meta>
-      <span className='icehockey'></span>
-    </Card.Meta>
-    <Card.Description>icehockey</Card.Description>
-  </Card.Content>
-  <Card.Content extra>
-  
-  </Card.Content>
-</Card>
-
-
-<Card>
-    <Container >
-      <Image src={Football} />
-    </Container>
-    <Card.Content>
-      <Card.Header></Card.Header>
-      <Card.Meta>
-        <span className='football'></span>
-      </Card.Meta>
-      <Card.Description>Football</Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-    
-    </Card.Content>
-  </Card>
-  </Fragment>
-
-)
-
-export default Sport
+export default Sport;
