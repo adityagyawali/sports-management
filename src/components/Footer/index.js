3ec88844126d08186ccb0f9dd5ffe0830f6f4016
Aditya Gyawali <gyawali_aditya@hotmail.com>
2018-11-27 13:58:49 +0200

    test with jest and enzyme setup

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Container } from 'semantic-ui-react';
+import { Container } from "semantic-ui-react";
 import "./index.css";
 
 const Footer = () => {
-
-	return (<Container className="footer" fluid>
-			<span style={{"color":"white"}} >Copyright <span className="copyright">&copy;</span> 2018 Sports Management. All Rights Reserved.</span>
-			<br/>
+	return (
+		<Container className="footer" fluid>
+			<span style={{ color: "white" }}>
+				Copyright <span className="copyright">&copy;</span> 2018 Sports
+				Management. All Rights Reserved.
+			</span>
 			<br />
-			<span style={{"color":"white"}} >Follow us on:</span>
-			<a href="#"><i class="facebook icon"></i></a>
-			<a href="#"><i class="twitter icon"></i></a>
-			<a href="#"><i class="linkedin icon"></i></a>
-			<a href="#"><i class="google icon"></i></a>
-			<a href="#"><i class="skype icon"></i></a>
+			<br />
+			<span style={{ color: "white" }}>Follow us on:</span>
+			<a role="button">
+				<i className="facebook icon" />
+			</a>
+			<a href="#">
+				<i className="twitter icon" />
+			</a>
+			<a href="#">
+				<i className="linkedin icon" />
+			</a>
+			<a href="#">
+				<i className="google icon" />
+			</a>
+			<a href="#">
+				<i className="skype icon" />
+			</a>
 		</Container>
 	);
 };
