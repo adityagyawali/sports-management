6232efe36466d73246faae2b84131c58dec95409
unknown <latifu99i@gmail.com>
2018-12-03 00:43:13 +0200

    Addition sports to the catagory

--- src/components/SportSection/index.js
+++ src/components/SportSection/index.js
@@ -4,66 +4,43 @@ import Football from "../../assets/football.jpg";
 import basketball from "../../assets/basketball.jpg";
 import icehockey from "../../assets/icehockey.jpg";
 import golf from "../../assets/golf.jpg";
+
+import ski from "../../assets/ski.jpg";
+import cycle from "../../assets/cycle.jpg";
+import aerobics from "../../assets/aerobics.jpg";
+import usfootball from "../../assets/usfootball.jpg";
+import ball from "../../assets/ball.jpg";
+import tennis from "../../assets/tennis.jpg";
+import usfoo from "../../assets/usfoo.jpg";
+import badminton from "../../assets/badminton.jpg";
+
+
+
+
+
 import "./index.css";
 
 const Sport = () => (
 	<div className="sports">
-	<Card>
-			<Container>
-				<Image className="golf" src={golf} />
-			</Container>
-			<Card.Content>
-				<Card.Header />
-				<Card.Meta>
-					<span className="golf" />
-				</Card.Meta>
-				<Card.Description>GOLF</Card.Description>
-			</Card.Content>
-			<Card.Content extra />
-		</Card>  
-		<Card>
-			<Container>
-				<Image className="basketball" src={basketball} />
-			</Container>
-			<Card.Content>
-				<Card.Header />
-				<Card.Meta>
-					<span className="basketball" />
-				</Card.Meta>
-				<Card.Description>BASKETBALL</Card.Description>
-			</Card.Content>
-			<Card.Content extra />
-		</Card>  
-		<Card>
-			<Container>
-				<Image src={icehockey} />
-			</Container>
-			<Card.Content>
-				<Card.Header />
-				<Card.Meta>
-					<span className="icehockey" />
-				</Card.Meta>
-				<Card.Description>ICEHOCKEY</Card.Description>
-			</Card.Content>
-			<Card.Content extra />
-		</Card>
-
-		<Card>
-			<Container>
-				<Image src={Football} />
-			</Container>
-			<Card.Content>
-				<Card.Header />
-				<Card.Meta>
-					<span className="football" />
-				</Card.Meta>
-				<Card.Description>FOOTBALL</Card.Description>
-			</Card.Content>
-			<Card.Content extra />
-		</Card>
-
 		
+  <Card.Group itemsPerRow={4}>
+    <Card color='red'  image={golf} />
+    <Card color='orange' image={basketball} />
+    <Card color='yellow' image={Football} />
+    <Card color='olive' image={icehockey} />
+    <Card color='green' image={ski} />
+    <Card color='teal' image={cycle} />
+    <Card color='blue' image={aerobics} />
+    <Card color='violet' image={usfootball} />
+    <Card color='purple' image={ball} />
+    <Card color='pink' image={tennis} />
+    <Card color='brown' image={usfoo} />
+    <Card color='grey' image={badminton} />
+  </Card.Group>
+
 	</div>
+	
+
 );
 
 export default Sport;
