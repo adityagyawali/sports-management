715cfe3a8ff3c78eed9aa5f2bc6a5a1998ecdb0d
Aditya Gyawali <gyawali_aditya@hotmail.com>
2018-12-07 10:22:30 +0200

    authentication done

--- src/components/Authentication/Login/index.js
+++ src/components/Authentication/Login/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import {
 	Button,
 	Form,
@@ -8,53 +8,99 @@ import {
 	Message,
 	Segment
 } from "semantic-ui-react";
+import Axios from "axios";
 
-const LoginForm = () => (
-	<div className="login-form">
-		{/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-		<style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-		<Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
-			<Grid.Column style={{ maxWidth: 450 }}>
-				<Header as="h2" color="teal" textAlign="center">
-					Log-in to your account
-				</Header>
-				<Form size="large">
-					<Segment stacked>
-						<Form.Input
-							fluid
-							icon="user"
-							iconPosition="left"
-							placeholder="E-mail address"
-						/>
-						<Form.Input
-							fluid
-							icon="lock"
-							iconPosition="left"
-							placeholder="Password"
-							type="password"
-						/>
+class LoginForm extends React.Component {
+	state = {
+		email: "",
+		password: "",
+		error: ""
+	};
+	handleChane = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
 
-						<Button color="teal" fluid size="large">
-							Login
-						</Button>
-					</Segment>
-				</Form>
-				<Message>
-					New to us? <Link to="/register">Sign Up</Link>
-				</Message>
-			</Grid.Column>
-		</Grid>
-	</div>
-);
+	handleSubmit = e => {
+		e.preventDefault();
+		const { email, password } = this.state;
+		const loggedUser = {
+			email,
+			password
+		};
+		Axios.post("http://localhost:8000/api/user/login", loggedUser).then(res => {
+			if (res.status === 201) {
+				window.alert("username or password incorrect");
+			} else if (res.status === 200) {
+				this.props.history.push("/app");
+			} else {
+				window.alert("Login NOt successfull");
+			}
+		});
+	};
+	render() {
+		return (
+			<div className="login-form">
+				{/*
+		  Heads up! The styles below are necessary for the correct render of this example.
+		  You can do same with CSS, the main idea is that all the elements up to the `Grid`
+		  below must have a height of 100%.
+		*/}
+				<style>{`
+		  body > div,
+		  body > div > div,
+		  body > div > div > div.login-form {
+			height: 100%;
+		  }
+		`}</style>
+				<Grid
+					textAlign="center"
+					style={{ height: "100%" }}
+					verticalAlign="middle"
+				>
+					<Grid.Column style={{ maxWidth: 450 }}>
+						<Header as="h2" color="teal" textAlign="center">
+							Log-in to your account
+						</Header>
+						<Form size="large" onSubmit={this.handleSubmit}>
+							<Segment stacked>
+								<Form.Input
+									type="email"
+									required
+									fluid
+									icon="user"
+									iconPosition="left"
+									placeholder="E-mail address"
+									name="email"
+									onChange={this.handleChane}
+									value={this.state.email}
+								/>
+								<Form.Input
+									required
+									fluid
+									icon="lock"
+									iconPosition="left"
+									placeholder="Password"
+									type="password"
+									onChange={this.handleChane}
+									value={this.state.password}
+									name="password"
+								/>
 
-export default LoginForm;
+								<Button color="teal" fluid size="large">
+									Login
+								</Button>
+							</Segment>
+						</Form>
+						<Message>
+							New to us? <Link to="/register">Sign Up</Link>
+						</Message>
+					</Grid.Column>
+				</Grid>
+			</div>
+		);
+	}
+}
+
+export default withRouter(LoginForm);
--- src/components/Authentication/SignUp/index.js
+++ src/components/Authentication/SignUp/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import {
 	Button,
 	Form,
@@ -8,53 +8,103 @@ import {
 	Message,
 	Segment
 } from "semantic-ui-react";
+import axios from "axios";
 
-const SignupForm = () => (
-	<div className="signup-form">
-		{/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-		<style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.signup-form {
-        height: 100%;
-      }
-    `}</style>
-		<Grid textAlign="center" style={{ height: "100%" }} verticalAlign="middle">
-			<Grid.Column style={{ maxWidth: 450 }}>
-				<Header as="h2" color="teal" textAlign="center">
-					Create a new account
-				</Header>
-				<Form size="large">
-					<Segment stacked>
-						<Form.Input
-							fluid
-							icon="user"
-							iconPosition="left"
-							placeholder="E-mail address"
-						/>
-						<Form.Input
-							fluid
-							icon="lock"
-							iconPosition="left"
-							placeholder="Password"
-							type="password"
-						/>
+class SignupForm extends React.Component {
+	state = {
+		email: "",
+		password: "",
 
-						<Button color="teal" fluid size="large">
-							Sign Up
-						</Button>
-					</Segment>
-				</Form>
-				<Message>
-					Already a member? <Link to="/login">Login</Link>
-				</Message>
-			</Grid.Column>
-		</Grid>
-	</div>
-);
+		isLoading: false
+	};
 
-export default SignupForm;
+	handleChange = e => {
+		e.preventDefault();
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const { email, password } = this.state;
+		const userData = {
+			email,
+			password
+		};
+		this.setState({
+			isLoading: true
+		});
+		axios
+			.post("http://localhost:8000/api/user/register", userData)
+			.then(res => {
+				if (res.status === 200) {
+					this.props.history.push("/login");
+				}
+			});
+	};
+	render() {
+		return (
+			<div className="signup-form">
+				{/*
+		  Heads up! The styles below are necessary for the correct render of this example.
+		  You can do same with CSS, the main idea is that all the elements up to the `Grid`
+		  below must have a height of 100%.
+		*/}
+				<style>{`
+		  body > div,
+		  body > div > div,
+		  body > div > div > div.signup-form {
+			height: 100%;
+		  }
+		`}</style>
+				<Grid
+					textAlign="center"
+					style={{ height: "100%" }}
+					verticalAlign="middle"
+				>
+					<Grid.Column style={{ maxWidth: 450 }}>
+						<Header as="h2" color="teal" textAlign="center">
+							Create a new account
+						</Header>
+						<Form size="large" onSubmit={this.handleSubmit}>
+							<Segment stacked>
+								<Form.Input
+									type="email"
+									required
+									fluid
+									icon="user"
+									iconPosition="left"
+									placeholder="E-mail address"
+									name="email"
+									onChange={this.handleChange}
+									value={this.state.email}
+								/>
+								<Form.Input
+									required
+									fluid
+									icon="lock"
+									iconPosition="left"
+									placeholder="Password"
+									type="password"
+									name="password"
+									onChange={this.handleChange}
+									value={this.state.password}
+								/>
+
+								<Button color="teal" fluid size="large">
+									Sign Up
+								</Button>
+							</Segment>
+						</Form>
+						<Message>
+							Already a member? <Link to="/login">Login</Link>
+						</Message>
+					</Grid.Column>
+				</Grid>
+			</div>
+		);
+	}
+}
+
+export default withRouter(SignupForm);
